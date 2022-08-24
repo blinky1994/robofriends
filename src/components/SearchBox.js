b8f86396df77c9050f0960616932df3b6abd7559
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-08-24 16:45:03 +0800

    optimzing for pwa

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchChange}) => {
 	return (
 		<div className='pa2 ma2'>
 		<input 
+		aria-label='Search Robots'
 		className = 'br3-ns pa3 ba b--green bg-lightest-blue'
 		type='search' 
 		placeholder='search robots'
